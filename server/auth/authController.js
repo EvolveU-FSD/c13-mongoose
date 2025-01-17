4e3118ef722832a5fe0087b2469777b49d76b7d3
Tony Enerson <tony@enerson.ca>
2025-01-17 13:09:34 -0700

    Shrink auth middleware

--- server/auth/authController.js
+++ server/auth/authController.js
@@ -3,31 +3,27 @@ import { checkPassword } from "../user/userData.js"
 
 const router = Router()
 
+async function getUserFromRequest(req) {
+    let user
+    if (req.headers['authorization']) {
+        const authorization = req.headers['authorization']
+        const [scheme, credential] = authorization.split(' ')
+        if (scheme === 'Basic') {
+            const usernamePassword = Buffer.from(credential, 'base64').toString()
+            const [username, password] = usernamePassword.split(':')
+            user = await checkPassword(username, password)
+        }        
+    }
+    return user
+}
+
 export async function requireBasicAuth(req, res, next) {
+    req.user = await getUserFromRequest(req)
     // send back 401 if there is no authentication header
-    if (!req.headers['authorization']) {
-        console.log('No authorization header present, responding with 401')
+    if (!req.user) {
         res.sendStatus(401)
         return
     }
-    const authorization = req.headers['authorization']
-    const [scheme, credential] = authorization.split(' ')
-    if (scheme !== 'Basic') {
-        res.set('WWW-Authenticate', 'Basic')
-        res.sendStatus(401)
-        return
-    }
-    const usernamePassword = Buffer.from(credential, 'base64').toString()
-    const [username, password] = usernamePassword.split(':')
-
-    // do the check here!
-    const user = await checkPassword(username, password)
-
-    if (!user) {
-        res.sendStatus(401)
-        return
-    }
-    req.user = user        
     next()
 }
 
@@ -51,16 +47,7 @@ router.post('/login', async function (req, res) {
 })
 
 router.get('/currentUser', async function (req, res) {
-    let user 
-    if (req.headers['authorization']) {
-        const authorization = req.headers['authorization']
-        const [scheme, credential] = authorization.split(' ')
-        if (scheme === 'Basic') {
-            const usernamePassword = Buffer.from(credential, 'base64').toString()
-            const [username, password] = usernamePassword.split(':')
-            user = await checkPassword(username, password)
-        }        
-    }
+    let user = await getUserFromRequest(req)
     res.send(user)
 })
 
